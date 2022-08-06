f48feda7f2b1f41eaba479efa8ef089e460987f9
zddd-hgs <44159598+zddd-hgs@users.noreply.github.com>
2022-08-06 16:27:02 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  6 15:46:04 2022
+
+@author: zhudan
+"""
+
+import streamlit as st
+import pandas as pd
+
+def main():
+    st.title = "This is a title"
+    st.header = "This is a header"
+    st.subheader = "This is a subheader"
+    
+
+    
+if __name__ == '__main__':
+    main()
